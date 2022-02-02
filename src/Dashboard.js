f3d2e2fbce483fd610a5b9546c4a01ffb424e472
Denis Granha <denisgranha@gmail.com>
2022-02-02 13:09:41 +0100

    refactor code and adapt nft card. Use temporal data with real images and ipfs links

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,140 +1,63 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
-import Color from "color"; // v3.2.1
-import { styled } from "@mui/material/styles";
-import Card from "@mui/material/Card";
-import CardActionArea from "@mui/material/CardActionArea";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
+
 import Grid from "@mui/material/Grid";
-import Typography from "@mui/material/Typography";
 
-
-const CardActionAreaActionArea = styled(CardActionArea)(() => ({
-  borderRadius: 16,
-  transition: "0.2s",
-  "&:hover": {
-    transform: "scale(1.1)",
-  },
-}));
-
-const StyledCard = styled(Card)(({ color }) => ({
-  minWidth: 256,
-  borderRadius: 16,
-  boxShadow: "none",
-  "&:hover": {
-    boxShadow: `0 6px 12px 0 ${Color(color).rotate(-12).darken(0.2).fade(0.5)}`,
-  },
-}));
-
-const CardContentContent = styled(CardContent)(({ color }) => {
-  return {
-    backgroundColor: color,
-    padding: "1rem 1.5rem 1.5rem",
-  };
-});
-
-const TypographyTitle = styled(Typography)(() => ({
-  fontFamily: "Keania One",
-  fontSize: "2rem",
-  color: "#fff",
-  textTransform: "uppercase",
-}));
-
-const TypographySubtitle = styled(Typography)(() => ({
-  fontFamily: "Montserrat",
-  color: "#fff",
-  opacity: 0.87,
-  marginTop: "2rem",
-  fontWeight: 500,
-  fontSize: 14,
-}));
-
-const CustomCard = ({
-  color,
-  image,
-  title,
-  subtitle,
-  destinationPath
-}) => {
-  const history = useNavigate();
-  
-  return (
-  <CardActionAreaActionArea onClick={() => {
-    history(destinationPath)
-  }}>
-    <StyledCard color={color}>
-      <CardMedia
-        image={image}
-        sx={{
-          width: "100%",
-          height: 0,
-          paddingBottom: "75%",
-          backgroundColor: "rgba(0,0,0,0.08)",
-        }}
-      />
-      <CardContentContent color={color}>
-        <TypographyTitle variant={"h2"}>{title}</TypographyTitle>
-        <TypographySubtitle>{subtitle}</TypographySubtitle>
-      </CardContentContent>
-    </StyledCard>
-  </CardActionAreaActionArea>
-)};
+import NFTCard from "./components/NFTCard";
 
 export default function Dashboard() {
 
+  let NFTs = [
+    {
+      id: 0,
+      title: "What A Show",
+      description: "Lorem ipsum",
+      color: "#203f52",
+      image: "https://ipfs.io/ipfs/QmTzGUNs5qu6HDhJXuGgQBf4wMNeFS5yRcuTotoVXyXy2v?preview=1"
+    },
+    {
+      id: 1,
+      title: "What A Show",
+      description: "Lorem ipsum",
+      color: "#4d137f",
+      image: "https://ipfs.io/ipfs/QmPxKzaMPsyfq7ZFR7SdZwnSkFq2kzfDHWUWJP3zimRhN9?preview=1"
+    },
+    {
+      id: 2,
+      title: "How To Stop Time",
+      description: "Lorem ipsum",
+      color: "#ff9900",
+      image: "https://ipfs.io/ipfs/QmPvwxB7rKQLqo6VXo6a8toxgy1eqb3E185kj3H1VwgnWK?preview=1"
+    },
+    {
+      id: 3,
+      title: "Everything I Did",
+      description: "Lorem ipsum",
+      color: "#34241e",
+      image: "https://ipfs.io/ipfs/QmQ7EFQVqTg65Vhr3ACtNEmbwGMuNSfCqueU7GtYj91rxy?preview=1"
+    },
+    {
+      id: 4,
+      title: "Everything I Did",
+      description: "Lorem ipsum",
+      color: "#000000",
+      image: "https://ipfs.io/ipfs/QmTv6dhYfF3sZNnua3oEE2A3B1sYm2o8nmKE6ANpjjTpJF?preview=1"
+    },
+
+  ]
+
   return (
     <Grid container spacing={4}>
-      <Grid item>
-        <CustomCard
-          destinationPath="/nft1"
-          color={"#203f52"}
-          title={"Dota 2"}
-          subtitle={"Be a Legend!"}
-          image={
-            "https://steamcdn-a.akamaihd.net/apps/dota2/images/blog/play/dota_heroes.png"
-          }
-        />
-        
-      </Grid>
-      <Grid item>
-        <CustomCard
-          color="#4d137f"
-          title={"Fortnite"}
-          subtitle={"Time to choose side!"}
-          image={
-            "https://static1.millenium.org/articles/0/31/21/20/@/713589-calamity-article_list_m-1.jpg"
-          }
-        />
-      </Grid>
-      <Grid item>
-        <CustomCard
-          color="#ff9900"
-          title={"Overwatch"}
-          subtitle={"What are you waiting?"}
-          image={"https://images5.alphacoders.com/690/thumb-1920-690653.png"}
-        />
-      </Grid>
-      <Grid item>
-        <CustomCard
-          color="#34241e"
-          title={"PUBG"}
-          subtitle={"Are you ready?"}
-          image={
-            "https://www.itp.net/public/styles/full_img_sml/public/images/2019/05/27/44485-pubg_base1.jpg?itok=EF911Xan"
-          }
-        />
-      </Grid>
-      <Grid item>
-        <CustomCard
-          color={"#000000"}
-          title={"Rumia"}
-          subtitle={"Be an Artist!"}
-          image={
-            "https://www.fluctuarecords.com/static/312bb8574111e551f82dc7e5450e1d07/51025/rumia-album.jpg"
-          }
-        />
-      </Grid>
+      {NFTs.map(nft => (
+        <Grid item xs={12} sm={6} lg={4}>
+          <NFTCard
+            destinationPath={`/nft/${nft.id}`}
+            color={nft.color}
+            title={nft.title}
+            subtitle={nft.description}
+            image={nft.image}
+          />
+        </Grid>
+      ))}
     </Grid>
   );
 }
---
+++ src/components/NFTCard.js
@@ -0,0 +1,81 @@
+import { useNavigate } from "react-router-dom";
+import Color from "color"; // v3.2.1
+import { styled } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardActionArea from "@mui/material/CardActionArea";
+// import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+// import Typography from "@mui/material/Typography";
+
+const CardActionAreaActionArea = styled(CardActionArea)(() => ({
+    borderRadius: 16,
+    transition: "0.2s",
+    "&:hover": {
+      transform: "scale(1.1)",
+    },
+  }));
+  
+  const StyledCard = styled(Card)(({ color }) => ({
+    minWidth: 256,
+    borderRadius: 16,
+    boxShadow: "none",
+    "&:hover": {
+      boxShadow: `0 6px 12px 0 ${Color(color).rotate(-12).darken(0.2).fade(0.5)}`,
+    },
+  }));
+  
+//   const CardContentContent = styled(CardContent)(({ color }) => {
+//     return {
+//       backgroundColor: color,
+//       padding: "1rem 1.5rem 1.5rem",
+//     };
+//   });
+  
+//   const TypographyTitle = styled(Typography)(() => ({
+//     fontFamily: "Keania One",
+//     fontSize: "2rem",
+//     color: "#fff",
+//     textTransform: "uppercase",
+//   }));
+  
+//   const TypographySubtitle = styled(Typography)(() => ({
+//     fontFamily: "Montserrat",
+//     color: "#fff",
+//     opacity: 0.87,
+//     marginTop: "2rem",
+//     fontWeight: 500,
+//     fontSize: 14,
+//   }));
+  
+  const NFTCard = ({
+    color,
+    image,
+    title,
+    subtitle,
+    destinationPath
+  }) => {
+    const history = useNavigate();
+    
+    return (
+    <CardActionAreaActionArea onClick={() => {
+      history(destinationPath)
+    }}>
+      <StyledCard color={color}>
+        <CardMedia
+          image={image}
+          sx={{
+            width: "100%",
+            height: 0,
+            paddingBottom: "100%",
+            backgroundColor: "rgba(0,0,0,0.08)",
+          }}
+        />
+        {/* <CardContentContent color={color}>
+          <TypographyTitle variant={"h2"}>{title}</TypographyTitle>
+          <TypographySubtitle>{subtitle}</TypographySubtitle>
+        </CardContentContent> */}
+      </StyledCard>
+    </CardActionAreaActionArea>
+  )};
+
+  export default NFTCard;
