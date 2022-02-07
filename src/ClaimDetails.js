57959df7770565046ef7432a038822b66b3de698
Denis Granha <denisgranha@gmail.com>
2022-02-07 13:08:04 +0100

    get spotify fallback url and use english as language

--- src/ClaimDetails.js
+++ src/ClaimDetails.js
@@ -3,12 +3,12 @@ import {
     useParams
 } from "react-router-dom";
 
-const baseURL = "https://accounts.spotify.com/es/authorize?"
+const baseURL = "https://accounts.spotify.com/en/authorize?"
 let paramsObj = {
     scope: 'user-library-modify user-read-email',
     response_type: 'code',
     client_id: '92fec7cd91604f69a1d9a2327d7c515c',
-    redirect_uri: 'http://localhost:3000/mint/'
+    redirect_uri: `${window.location.origin}/mint/`
 };
 
 export default function ClaimDetails(){
