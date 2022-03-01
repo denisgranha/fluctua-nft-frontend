5975c8e55060081e97d449248ad1e92a87230977
Denis Granha <denisgranha@gmail.com>
2022-03-01 18:00:55 +0100

    add claim infographics as pngs instead of svg

--- src/ClaimDetails.js
+++ src/ClaimDetails.js
@@ -3,9 +3,10 @@ import { Divider } from "@mui/material";
 import { Grid } from "@mui/material";
 import { Typography } from "@mui/material";
 
-import {ReactComponent as Infographic4} from "./svgs/Asset4.svg"
-import {ReactComponent as Infographic5} from "./svgs/Asset5.svg"
-import {ReactComponent as Infographic6} from "./svgs/Asset6.svg"
+// import {ReactComponent as Infographic4} from "./svgs/Asset4.svg"
+// import {ReactComponent as Infographic5} from "./svgs/Asset5.svg"
+// import {ReactComponent as Infographic6} from "./svgs/Asset6.svg"
+import {NFTCardWithoutLink} from "./components/NFTCard"
 
 import {
     useParams
@@ -36,19 +37,22 @@ export default function ClaimDetails(){
             </Typography>
             <Grid container spacing={4}   justifyContent="center" style={{paddingTop: "2rem"}}>
                 <Grid item xs={12} sm={12} lg={3} xl={3}>
-                    <Infographic4 style={{height: "30vh"}}></Infographic4>
+                    {/* <Infographic4 style={{height: "30vh"}}></Infographic4> */}
+                    <NFTCardWithoutLink image="/img/NFTwebsite-05.png"></NFTCardWithoutLink>
                     <Typography variant="h6" component="div">
                         first, you would need to log into your Spotify account and allow us access to pre-save RUMIAs new single
                     </Typography>
                 </Grid>   
                 <Grid item xs={12} sm={12} lg={3} xl={3}>
-                    <Infographic5 style={{height: "30vh"}}></Infographic5>
+                    {/* <Infographic5 style={{height: "30vh"}}></Infographic5> */}
+                    <NFTCardWithoutLink image="/img/NFTwebsite-06.png"></NFTCardWithoutLink>
                     <Typography variant="h6" component="div">
                         then, you can sign up to Fortmatic to set up your digital wallet.
                     </Typography>
                 </Grid>   
                 <Grid item xs={12} sm={12} lg={3} xl={3}>
-                    <Infographic6 style={{height: "30vh"}}></Infographic6>
+                    {/* <Infographic6 style={{height: "30vh"}}></Infographic6> */}
+                    <NFTCardWithoutLink image="/img/NFTwebsite-07.png"></NFTCardWithoutLink>
                     <Typography variant="h6" component="div">
                         after these steps are completed, we will generate your NFT! <br/>
                         you can now unlock your exclusive content!
